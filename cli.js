7a00a874a4e66c608813840ace406ca3def8d7ff
Tom Ruff <ruffmantom@gmail.com>
2020-02-03 11:34:22 -0700

    added some logic for the if else to get the answers

--- cli.js
+++ cli.js
@@ -22,21 +22,12 @@ var positionSelect = [
 // if they want to continue to add more or end session
 
 // Questions for that specific position
-function employeeQues() {
-    employee = {
-        type: "input",
-        name: "username",
-        message: "What is your GitHub username?"
-
-    }
-    return employee;
-}
 function internQues() {
     intern = [
         {
             type: "input",
             name: "name",
-            message: "What is the applicants Name?"
+            message: "What is the Intern's Name?"
         },
         {
             type: "input",
@@ -45,25 +36,83 @@ function internQues() {
         },
         {
             type: "input",
-            name: "username",
+            name: "email",
             message: "What is thier Email?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the school they are attending?"
         }]
     return intern;
+
 }
+function employeeQues() {
+    employee = [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Employee's Name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is thier ID number?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is thier Email?"
+        }]
+    return employee;
+}
+
 function engineerQues() {
-    engineer = {
-        type: "input",
-        name: "username",
-        message: "What is your GitHub username?"
-    }
+    engineer = [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Engineer's Name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is thier ID number?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is thier Email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is thier GitHub?"
+        }]
     return engineer;
 }
 function managerQues() {
-    manager = {
-        type: "input",
-        name: "username",
-        message: "What is your GitHub username?"
-    }
+    manager = [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Manager's Name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is thier ID number?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is thier Email?"
+        },
+        {
+            type: "input",
+            name: "officeNum",
+            message: "What is the curent office number?"
+        }]
     return manager;
 }
 
@@ -77,7 +126,44 @@ promtUser()
     .then(function (res) {
         // console.log(res)
         if (res.position === 'Intern') {
-
-            inquirer.prompt(internQues());
+            inquirer.prompt(internQues())
+                .then(answer => {
+                    const name = answer.name;
+                    const id = answer.id;
+                    const email = answer.email;
+                    const school = answer.school;
+                    const cardArray = [name, id, email, school];
+                    console.log(cardArray)
+                })
+        } else if (res.position === 'Employee') {
+            inquirer.prompt(employeeQues())
+                .then(answer => {
+                    const name = answer.name;
+                    const id = answer.id;
+                    const email = answer.email;
+                    const cardArray = [name, id, email];
+                    console.log(cardArray)
+                })
+        } else if (res.position === 'Engineer') {
+            inquirer.prompt(engineerQues())
+                .then(answer => {
+                    const name = answer.name;
+                    const id = answer.id;
+                    const email = answer.email;
+                    const github = answer.github;
+                    const cardArray = [name, id, email, github];
+                    console.log(cardArray)
+                })
+        } else if (res.position === 'Manager') {
+            inquirer.prompt(managerQues())
+                .then(answer => {
+                    const name = answer.name;
+                    const id = answer.id;
+                    const email = answer.email;
+                    const officeNum = answer.officeNum;
+                    const cardArray = [name, id, email, officeNum];
+                    console.log(cardArray)
+                })
         }
+
     })
